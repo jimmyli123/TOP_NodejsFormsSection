19219fc71b2d2c418216e42d7a012c3b2c52aaef
Jimmy <lijjimmy@gmail.com>
2024-07-31 21:47:10 -0400

    TOP course had an update. Revising code to match the course update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,31 +1,34 @@
 // controllers/usersController.js
 const asyncHandler = require("express-async-handler");
 const usersStorage = require("../storages/usersStorage");
-
 const { body, validationResult } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
 
 const validateUser = [
-  body("firstName")
-    .trim()
-    .isAlpha()
-    .withMessage(`First name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 })
-    .withMessage(`First name ${lengthErr}`),
-  body("lastName")
-    .trim()
-    .isAlpha()
-    .withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 })
-    .withMessage(`Last name ${lengthErr}`),
-  body("age")
-    .trim()
-    .isInt({ gt: 1, lt: 150 })
-    .withMessage("Only numbers allowed"),
-  body("email").trim().isEmail().isLength({ min: 5, max: 25 }),
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
 ];
+
+
+exports.usersListGet = asyncHandler(async (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+});
+
+exports.usersCreateGet = asyncHandler(async (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+});
+
 
 // We can pass an entire array of middleware validations to our controller.
 exports.usersCreatePost = [
@@ -33,49 +36,44 @@ exports.usersCreatePost = [
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).render("users", {
-        title: "User List",
+      return res.status(400).render("createUser", {
+        title: "Create user",
         errors: errors.array(),
       });
     }
-    const { firstName, lastName, age, bio, email  } = req.body;
-    usersStorage.addUser({ firstName, lastName, age, bio, email });
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
     res.redirect("/");
-  }),
+  })
 ];
 
-exports.usersCreateGet = asyncHandler(async (req, res) => {
-  res.render("users", {
-    title: "User List",
-    users: usersStorage.getUsers(),
-  });
-});
 
 exports.usersUpdateGet = asyncHandler(async (req, res) => {
   const user = usersStorage.getUser(req.params.id);
-  res.render("update", { user, errors: [] });
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
 });
-
 exports.usersUpdatePost = [
   validateUser,
   asyncHandler(async (req, res) => {
     const user = usersStorage.getUser(req.params.id);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).render("update", {
-        errors: errors.array(),
+      return res.status(400).render("updateUser", {
+        title: "Update user",
         user: user,
+        errors: errors.array(),
       });
     }
-    const { firstName, lastName, age, email } = req.body;
-    usersStorage.updateUser(req.params.id, { firstName, lastName, age, email });
+    const { firstName, lastName } = req.body;
+    usersStorage.updateUser(req.params.id, { firstName, lastName });
     res.redirect("/");
-  }),
+  })
 ];
-
 // Tell the server to delete a matching user, if any. Otherwise, respond with an error.
 exports.usersDeletePost = asyncHandler(async (req, res) => {
-  const user = usersStorage.getUser(req.params.id);
   usersStorage.deleteUser(req.params.id);
   res.redirect("/");
 });
--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -1,38 +1,33 @@
 // storages/usersStorage.js
 // This class lets us simulate interacting with a database.
 class UsersStorage {
-    constructor() {
-      this.storage = {};
-      this.id = 0;
-      this.age = 1;
-      this.bio = "";
-      this.email = "";
-      
-    }
-  
-    addUser({firstName, lastName, age, bio, email}) {
-      const id = this.id;
-      this.storage[id] = { id, firstName, lastName, age, bio, email  };
-      this.id++;
-    }
-  
-    getUsers() {
-      return Object.values(this.storage);
-    }
-  
-    getUser(id) {
-      return this.storage[id];
-    }
-  
-    updateUser(id, {firstName, lastName, age, email }) {
-      this.storage[id] = { id, firstName, lastName, age, email  };
-    }
-  
-    deleteUser(id) {
-      delete this.storage[id];
-    }
+  constructor() {
+    this.storage = {};
+    this.id = 0;
   }
-  // Rather than exporting the class, we can export an instance of the class by instantiating it.
-  // This ensures only one instance of this class can exist, also known as the "singleton" pattern.
-  module.exports = new UsersStorage();
-  
+
+  addUser({ firstName, lastName }) {
+    const id = this.id;
+    this.storage[id] = { id, firstName, lastName };
+    this.id++;
+  }
+
+  getUsers() {
+    return Object.values(this.storage);
+  }
+
+  getUser(id) {
+    return this.storage[id];
+  }
+
+  updateUser(id, { firstName, lastName }) {
+    this.storage[id] = { id, firstName, lastName };
+  }
+
+  deleteUser(id) {
+    delete this.storage[id];
+  }
+}
+// Rather than exporting the class, we can export an instance of the class by instantiating it.
+// This ensures only one instance of this class can exist, also known as the "singleton" pattern.
+module.exports = new UsersStorage();
